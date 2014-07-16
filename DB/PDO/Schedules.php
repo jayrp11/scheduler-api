7140e72ffcdbbcd4fe8548611c2f60957e52a47a
Jaydeep <jaydeep@jdp.(none)>
2014-07-16 20:49:52 +1000

    moved db details

--- DB/PDO/Schedules.php
+++ DB/PDO/Schedules.php
@@ -2,33 +2,14 @@
 
 use Luracast\Restler\RestException;
 
-class DB_PDO_Schedules
+class DB_PDO_Schedules extends DB_PDO_MySqlCRUD
 {
-    private $db;
     private $sub_schedules;
 
     function __construct()
     {
         try {
-            //Make sure you are using UTF-8
-            $options = array(PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8');
-
-            //Update the dbname username and password to suit your server
-            $this->db = new PDO(
-                'mysql:host=localhost;dbname=scheduler',
-                'root',
-                'root',
-                $options
-            );
-            $this->db->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE,
-                PDO::FETCH_ASSOC);
-
-            //If you are using older version of PHP and having issues with Unicode
-            //uncomment the following line
-            //$this->db->exec("SET NAMES utf8");
-
             $this->sub_schedules = new DB_PDO_SubSchedules();
-
         } catch (PDOException $e) {
             throw new RestException(501, 'MySQL: ' . $e->getMessage());
         }
