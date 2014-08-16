e2c1c3c885d2b42bbddcc8bd7d763201c30f99f9
Jaydeep <jaydeep@jdp.(none)>
2014-08-16 14:58:54 +1000

    removed dups

--- data/seed.sql
+++ data/seed.sql
@@ -1,4 +1,3 @@
-insert into resources(type, name) values('MUSIC', 'Keyboard');
 insert into resources(type, name) values('MUSIC', 'Harmonium');
 insert into resources(type, name) values('MUSIC', 'Khanzari');
 insert into resources(type, name) values('MUSIC', 'Manjira');
@@ -7,9 +6,7 @@ insert into resources(type, name) values('MUSIC', 'Duff');
 insert into resources(type, name) values('MUSIC', 'Keyboard 1');
 insert into resources(type, name) values('MUSIC', 'Keyboard 2');
 insert into resources(type, name) values('MUSIC', 'Koshijoda');
-insert into resources(type, name) values('MUSIC', 'Manjira');
 insert into resources(type, name) values('MUSIC', 'Octapad');
-insert into resources(type, name) values('MUSIC', 'Tabla');
 insert into resources(type, name) values('MUSIC', 'Tabla 1');
 insert into resources(type, name) values('MUSIC', 'Tabla 2');
 insert into resources(type, name) values('MUSIC', 'Guitar');
